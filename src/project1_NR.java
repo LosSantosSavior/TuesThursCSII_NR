a21a92ca13e0057cd22895f2c4d670338afb6175
LosSantosSavior <nickrussell001@gmail.com>
2021-09-18 16:17:55 -0400

    project1_NR 9/18/21

--- src/project1_NR.java
+++ src/project1_NR.java
@@ -2,9 +2,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class project1_NR {
     public static void main(String[] args) throws IOException {
+        var reader = new Scanner(System.in);
+        var userResponse = "";
         var name = new ArrayList<String>();
         var price = new ArrayList<Float>();
         var numAvailable = new ArrayList<Integer>();
@@ -19,6 +22,13 @@ public class project1_NR {
         }
         for (int i = 0; i <name.size(); i++)    {
             System.out.println("Product: " + name.get(i) + "\nPrice: " + price.get(i) + "\nStock available: " + numAvailable.get(i));
+        }
+        while (!userResponse.equals("done"))    {
+            System.out.println("Type in the item you want to look for. Type 'done' if you do not want to look for any more items.");
+            userResponse = reader.nextLine();
+            if (!userResponse.equals("done"))   {
+                name.add(userResponse);
+            }
         }
     }
 }
