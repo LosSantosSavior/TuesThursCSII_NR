7ea32e9c0e0871c21fc0775b94ffeb1f88af6f03
LosSantosSavior <nickrussell001@gmail.com>
2021-09-21 15:45:07 -0400

    project1_NR 9/21/21

--- src/project1_NR.java
+++ src/project1_NR.java
@@ -14,30 +14,27 @@ public class project1_NR {
         var fileName = "products.txt";
         var filePath = Paths.get(fileName);
         var allLines = Files.readAllLines(filePath);
-        for (var line: allLines) {
+
+        for (var line : allLines) {
             var splitLine = line.split(",");
             name.add(splitLine[0]);
             price.add(Float.parseFloat(splitLine[1]));
             numAvailable.add(Integer.parseInt(splitLine[2]));
         }
-        for (int i = 0; i <name.size(); i++)    {
+
+        for (int i = 0; i < name.size(); i++) {
             System.out.println("Product: " + name.get(i) + "\nPrice: " + price.get(i) + "\nStock available: " + numAvailable.get(i) + "\n");
         }
-        while (!userResponse.equals("done"))    {
+
+        while (!userResponse.equals("done")) {
             System.out.println("Type in the item you want to look for. Type 'done' if you do not want to look for any more items.");
             userResponse = reader.nextLine();
             userResponse = userResponse.toLowerCase();
-
-            switch (userResponse) {
-                case "doritos" -> name.get(1);
-                case "oreos" -> name.get(2);
-                case "baked beans" -> name.get(3);
-                case "soda" -> name.get(4);
-                case "cake" -> name.get(5);
-                case "acne cream" -> name.get(6);
-                case "done" -> System.exit(0);
-                default -> System.out.println("Invalid item.");
+            for (int i = 0; i < name.size(); i++) {
+                if (userResponse.equals(name.get(i))) {
+                    System.out.println("Product: " + name.get(i) + "\nPrice: " + price.get(i) + "\nStock available: " + numAvailable.get(i) + "\n");
                 }
             }
         }
     }
+}
